1199b28434248b310b97f0c95c1c67dea505d019
Kirill <39500335+IllidanStormrage1@users.noreply.github.com>
2018-08-08 19:58:14 +0300

    Update get_proxy_list.py

--- get_proxy_list.py
+++ get_proxy_list.py
@@ -2,24 +2,23 @@ import bs4
 import requests
 import os
 
-
 def get_html(url):
     r = requests.get(url)
     return r.text
 
-
+# проверяем наличие файла , если он есть - удаляем.
 def check_file():
     try:
         os.remove("proxies.txt")
     except:
         pass
 
-
+# записываем наши прокси в текстовый файл
 def write(data):
     with open("proxies.txt", "a+") as f:
         f.write(data + "\n")
 
-
+# получение всех ip, в ads ~200 айпишников . При первом запросе они сразу все загружаются.
 def get_page_data(html):
     soup = bs4.BeautifulSoup(html, "lxml")
 
@@ -34,7 +33,7 @@ def get_page_data(html):
             port = ad.find_all("td")[1].text
         except:
             port = ""
-
+        # формируем полный адрес и вызываем функцию записи
         data = str(ip) + ":" + str(port)
         write(data)
 
