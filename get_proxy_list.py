c5ba0aefdfb33f15fed13ad85b822515d9378d56
Kirill zudov <ueurhfyh1@gmail.com>
2018-12-30 16:34:31 +0300

    Update get_proxy_list.py

--- get_proxy_list.py
+++ get_proxy_list.py
@@ -10,15 +10,16 @@ def get_html(url):
 
 def write(data_set):
     '''Записываем наши прокси в текстовый файл'''
-    with open("proxies.txt", "a+") as f:
+    with open("proxies.txt", "w") as f:
         for i in data_set:
             f.write(i + '\n')
 
 
 def get_page_data(html):
-    '''Получение всех ip'''
     global data_set
+    '''Получение всех ip, в ads ~200 айпишников'''
     soup = bs4.BeautifulSoup(html, "lxml")
+
     ads = soup.find("table").find("tbody").find_all("tr")
     data_set = []
 
@@ -35,8 +36,9 @@ def get_page_data(html):
 
 
 def main():
-    url = "https://www.us-proxy.org/"
-    get_page_data(get_html(url))
+    site = int(input("0 - HTTP/HTTPS\n1 - Socks4\n"))
+    url = ["https://www.us-proxy.org/", "https://www.socks-proxy.net/"]
+    get_page_data(get_html(url[site]))
     write(data_set)
 
 
